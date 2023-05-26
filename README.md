8a486b5c9c2268e63f8d40aa7ffa4b2073f2e590
Daniel Farias <daniel.farias@santodigital.com.br>
2023-05-26 19:27:39 -0300

    Add cloud build config

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # demo-front
+
+```bash
+docker run -t -p 8080:8080 --env BACK_END_URL=http://localhost:8000 front-demo:v1
+```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
